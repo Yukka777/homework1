6e20b7ec6253ce2476d67c80b4451c4f575cc65a
YuGarmay <YuGarmay@kaluga.beeline.ru>
2024-11-06 23:59:14 +0300

    Task 5

---
+++ Car/Car.java
@@ -0,0 +1,22 @@
+package Car.src.main.java.org.example;
+
+public class Car implements Car.src.main.java.org.example.Drivable {
+    private String mark;
+    private String model;
+    private int year;
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void drive() {
+
+    }
+}
---
+++ Car/Drivable.java
@@ -0,0 +1,6 @@
+package Car.src.main.java.org.example;
+public interface Drivable {
+    public void  start();
+    public void  stop();
+    public void  drive(distance);
+}
